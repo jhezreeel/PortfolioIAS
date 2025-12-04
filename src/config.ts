e27a14ce2f20c8e0c014dc21a91f9c5c9fe48bf2
jzrl <145742232+jhezreeel@users.noreply.github.com>
2025-12-04 19:13:04 +0800

    Add files via upload

---
+++ src/config.ts
@@ -0,0 +1,78 @@
+export const siteConfig = {
+  name: "Jhezreel Rodge M. Amabilis",
+  title: "An Aspiring Web and App Developer",
+  description: "Jhezreel Rodge M. Amabilis Portfolio",
+  accentColor: "#1d4ed8",
+  social: {
+    email: "jc.amabilis@gmail.com",
+    linkedin: "https://www.linkedin.com/in/jhezreel-amabilis-81242618b/",
+    twitter: "https://x.com/jhezreeel",
+    github: "https://github.com/jhezreeel",
+  },
+  aboutMe:
+    "Hi! I’m Jhezreel Rodge M. Amabilis, an aspiring Web and App Developer with a strong passion for creating clean, responsive, and user-friendly digital experiences. I thrive on turning ideas into interactive websites and mobile applications that solve real problems and make technology accessible to everyone. I constantly explore new tools, frameworks, and programming languages to expand my skill set and stay up-to-date with the latest trends in development. I believe that technology should not only be functional but also intuitive, engaging, and enjoyable to use. My goal is to build applications and websites that leave a positive impact on users, whether by simplifying tasks, enhancing productivity, or providing seamless digital experiences. Driven by curiosity, creativity, and a desire to make meaningful contributions, I aim to grow into a well-rounded developer who can bring innovative ideas to life.",
+  skills: ["Javascript", "React", "Node.js", "Python", "HTML5", "CSS", "MySQL"],
+
+  projects: [
+    {
+      name: "Sales Management System",
+      description:
+        "A Sales Management System (SMS) is a tool that helps businesses track leads, manage customers, monitor sales, and automate tasks to make the sales process faster and more efficient.",
+      link: "https://github.com/KyooRuss/2BSIT-2-SalesManagement",
+      skills: ["JavaScript", "Typescript"],
+    },
+    {
+      name: "Flashcards Learning App (Active Recall) ",
+      description:
+        "A mobile app that lets users create, save, and study flashcards. Features include adding cards, organizing them into decks, flipping card animations, and a study mode with random shuffle.",
+      link: "https://snack.expo.dev/@neu_villamayor_kenneth_roy/flash-card-active-recall",
+      skills: ["React", "Node.js", "Snack Expo"],
+    },
+    {
+      name: "E-Commerce Web App",
+      description:
+        "A simple online store where users can browse products, view product details, add to cart, and checkout (mock checkout only).",
+      link: "https://extensionkit.io/?ref=devportfolio",
+      skills: ["HTML", "CSS.js", "Javascript", "MariaDB", "PHP"],
+    },
+  ],
+
+  insights: [
+    {
+      title: "Cybersecurity Journey: Hacking the Mindset",
+      description: "Insights from the journey of an aspiring IT professional, exploring ethical hacking, penetration testing, and real-world cybersecurity practices. Covers hands-on experiences with platforms like Hack The Box and TryHackMe, learning Bash, Python, encryption, SQL injection, XSS, and the importance of patience, integrity, and mentorship under Sir AJ.",
+      link: "https://jhezreeel.github.io/notes/note1.html",
+      date: "Nov 2025",
+    },
+    {
+      title: "Application Development (App Dev) Journey",
+      description: "A practical reflection on learning PHP, Bootstrap, Laravel, and database management with Laragon and Composer, highlighting the challenges and rewards of building fully functional web applications.",
+      link: "https://jhezreeel.github.io/notes/note2",
+      date: "Nov 2025",
+    },
+    {
+      title: "Human-Computer Interaction (HCI) Experience",
+      description: "An overview of learning UI/UX design using Figma, including wireframes, prototypes, and animations, and understanding the importance of user-friendly and aesthetically pleasing interfaces.",
+      link: "https://jhezreeel.github.io/notes/note3",
+      date: "Sep 2023",
+    },
+    {
+      title: "System Analysis Design SAD: From Analysis to Project Defense",
+      description: "Reflection on analyzing, designing, and planning systems using DFDs, ERDs, flowcharts, Warnier-O diagrams, Gantt charts, and low-fi UI/UX designs, culminating in creating and defending a Learning System",
+      link: "https://jhezreeel.github.io/notes/note4",
+      date: "December 2025",
+    },
+    {
+      title: "Information Management: Database Exploration",
+      description: "A reflection on managing databases like MariaDB, Oracle, and IBM, including connecting, activating, and utilizing them effectively, along with building and defending a system project under guidance from Sir Jerry Esperanza.",
+      link: "https://jhezreeel.github.io/notes/note5",
+      date: "Sept 2023",
+    },
+    {
+      title: "OOP: Coding with Structure and Purpose",
+      description: "Reflection on learning structured programming using OOP concepts like classes, objects, inheritance, encapsulation, and polymorphism, with hands-on experience creating a functional system connected to a database under Sir Elija Equiñas’ guidance.",
+      link: "https://jhezreeel.github.io/notes/note6",
+      date: "March 2023",
+    },
+  ],
+};
